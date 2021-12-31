fa50b2cdbb4c2d6b0f0a33dfdb566ff0d5027f37
jkong72 <96038721+jkong72@users.noreply.github.com>
2021-12-31 18:09:45 +0900

    commit

--- app.py
+++ app.py
@@ -53,10 +53,6 @@ def main() :
         st.title('컴퓨터 비전 프로젝트')
 
         # 파일 업로드
-        # dir_list = __file__.split('\\')
-        # temp_dir = '\\'.join(dir_list[:-1])
-        # temp_dir = temp_dir +'\\'+ 'temp'
-        # temp_dir = temp_dir.replace('\\', '\\\\')
         temp_dir = 'temp' # 파일 경로
 
         st.markdown('## 업로드 방식 선택')
